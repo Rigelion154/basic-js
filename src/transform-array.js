04417dee34d058e77d37b18734f26e0cc84cf322
Rigelion <cupofbeer123@gmail.com>
2022-10-17 17:03:25 +0300

    transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,60 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (arr.isArray)
+    return Error("'arr' parameter must be an instance of the Array!");
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let dn = "--discard-next";
+  let dp = "--discard-prev";
+  let dbn = "--double-next";
+  let dbp = "--double-prev";
+
+  if (arr.includes(dn) && arr.includes(dbp)) {
+    arr.splice(arr.indexOf(dn), 3);
+  }
+  if (arr.includes(dn) && arr.includes(dp)) {
+    arr.splice(arr.indexOf(dn), 3);
+  }
+  // if (arr.includes(dbn) && arr.includes(dp)) {
+  //   arr.splice(arr.indexOf(dbn), 3);
+  // }
+
+  if (arr.includes(dn)) {
+    arr.splice(arr.indexOf(dn), 2);
+  }
+  if (arr.includes(dbn)) {
+    let idbn = arr.indexOf(dbn);
+    if (idbn === arr.length - 1) arr.splice(idbn, 1);
+    else arr.splice(idbn, 1, arr[idbn + 1]);
+  }
+  if (arr.includes(dp)) {
+    let idp = arr.indexOf(dp);
+    if (idp === 0) arr.splice(0, 1);
+    else arr.splice(idp - 1, 2);
+  }
+
+  if (arr.includes(dbp)) {
+    let idbp = arr.indexOf(dbp);
+    if (idbp === 0) arr.splice(0, 1);
+    else arr.splice(idbp, 1, arr[idbp - 1]);
+  }
+  return arr;
 }
 
 module.exports = {
-  transform
+  transform,
 };
