fd56e2a9e2f9412c364ffc05bc586aa7dd465985
Rigelion <cupofbeer123@gmail.com>
2023-04-12 19:10:08 +0300

    feat: try to transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,48 +13,48 @@ const { NotImplementedError } = require("../extensions/index.js");
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/*arr*/) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  // if (arr.isArray)
-  //   return Error("'arr' parameter must be an instance of the Array!");
-  // // throw new NotImplementedError('Not implemented');
-  // // remove line with error and write your code here
-  // let dn = "--discard-next";
-  // let dp = "--discard-prev";
-  // let dbn = "--double-next";
-  // let dbp = "--double-prev";
-  //
-  // if (arr.includes(dn) && arr.includes(dbp)) {
-  //   arr.splice(arr.indexOf(dn), 3);
+  if (arr.isArray)
+    return Error("'arr' parameter must be an instance of the Array!");
+  // throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+  let dn = "--discard-next";
+  let dp = "--discard-prev";
+  let dbn = "--double-next";
+  let dbp = "--double-prev";
+
+  if (arr.includes(dn) && arr.includes(dbp)) {
+    arr.splice(arr.indexOf(dn), 3);
+  }
+  if (arr.includes(dn) && arr.includes(dp)) {
+    arr.splice(arr.indexOf(dn), 3);
+  }
+  // if (arr.includes(dbn) && arr.includes(dp)) {
+  //   arr.splice(arr.indexOf(dbn), 3);
   // }
-  // if (arr.includes(dn) && arr.includes(dp)) {
-  //   arr.splice(arr.indexOf(dn), 3);
-  // }
-  // // if (arr.includes(dbn) && arr.includes(dp)) {
-  // //   arr.splice(arr.indexOf(dbn), 3);
-  // // }
-  //
-  // if (arr.includes(dn)) {
-  //   arr.splice(arr.indexOf(dn), 2);
-  // }
-  // if (arr.includes(dbn)) {
-  //   let idbn = arr.indexOf(dbn);
-  //   if (idbn === arr.length - 1) arr.splice(idbn, 1);
-  //   else arr.splice(idbn, 1, arr[idbn + 1]);
-  // }
-  // if (arr.includes(dp)) {
-  //   let idp = arr.indexOf(dp);
-  //   if (idp === 0) arr.splice(0, 1);
-  //   else arr.splice(idp - 1, 2);
-  // }
-  //
-  // if (arr.includes(dbp)) {
-  //   let idbp = arr.indexOf(dbp);
-  //   if (idbp === 0) arr.splice(0, 1);
-  //   else arr.splice(idbp, 1, arr[idbp - 1]);
-  // }
-  // return arr;
+
+  if (arr.includes(dn)) {
+    arr.splice(arr.indexOf(dn), 2);
+  }
+  if (arr.includes(dbn)) {
+    let idbn = arr.indexOf(dbn);
+    if (idbn === arr.length - 1) arr.splice(idbn, 1);
+    else arr.splice(idbn, 1, arr[idbn + 1]);
+  }
+  if (arr.includes(dp)) {
+    let idp = arr.indexOf(dp);
+    if (idp === 0) arr.splice(0, 1);
+    else arr.splice(idp - 1, 2);
+  }
+
+  if (arr.includes(dbp)) {
+    let idbp = arr.indexOf(dbp);
+    if (idbp === 0) arr.splice(0, 1);
+    else arr.splice(idbp, 1, arr[idbp - 1]);
+  }
+  return arr;
 }
 
 module.exports = {
